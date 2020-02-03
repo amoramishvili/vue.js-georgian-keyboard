89dfde88b0f3eb81d436be844fa622322844610c
amo <37780913+amoramishvili@users.noreply.github.com>
2020-02-03 10:58:42 +0400

    Update vuejs_georgian_keyboard.js
    fixed event listener

--- vuejs_georgian_keyboard.js
+++ vuejs_georgian_keyboard.js
@@ -2,7 +2,7 @@ Vue.directive('georgian', {
 	// When the bound element is inserted into the DOM...
 	inserted: function (el) {
 		// Focus the element
-		el.addEventListener('keypress', (e) => {
+		el.addEventListener('keypress', function(e){
 			// console.log(e.which)
 			function setSelectionRange(input, selectionStart, selectionEnd) {
 				if (input.setSelectionRange) {
